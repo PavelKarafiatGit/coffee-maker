f5a90014e4cd3c576704099c1d052e031cbc99ed
PavelKarafiat <pavelkarafiat.git@gmail.com>
2023-02-14 12:05:22 +0100

    Základní struktura, main.py, source_data.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from source_data import MENU
+from source_data import resources
+
+### Základní nastavení
+
+
+
+
+### Funkce
+
+
+
+
+
+### Kód automatu
+user_choice = input("Co si dáte? (espresso/latte/cappuccino): ")
+
